418c068fffc5576bb47c60770df479383eac395b
jamezlz <35937551+jamezlz@users.noreply.github.com>
2021-04-23 13:31:17 -0500

    Basic functionality Done

---
+++ config/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    apiKey: "cbcdfb56-e1d3-466a-9fc2-1e0663983cf5"
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const axios = require('axios').default;
 const bodyParser = require('body-parser');
+const config = require("./config/config");
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
@@ -9,10 +10,32 @@ app.use(bodyParser.json());
 const PORT = 5000;
 const symbols = ["ETH", "BTC"];
 const dataUrl = "http://localhost:3000/positions";
+const priceUrl = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=";
 
 app.get("/", async (req,res)=>{
     try {
-        const positions = await axios.get(dataUrl);
+        let positions = await axios.get(dataUrl+"");
+        let cryptos = new Set();
+        for (let p of positions.data) {
+            cryptos.add(p.symbol);
+        }
+        let symbolString ="";
+        for(let c of cryptos) {
+            if (symbolString) {
+                symbolString += "," + c
+            } else {
+                symbolString += c
+            }
+        }
+        let prices = await axios.get(priceUrl+symbolString,{headers: {"X-CMC_PRO_API_KEY":config.apiKey}});
+        for (let p of positions.data) {
+            p.purchaseValue = p.purchasePrice * p.quantity;
+            p.currentPrice = prices.data.data[p.symbol].quote.USD.price.toFixed(2);
+            p.currentValue = (p.currentPrice * p.quantity).toFixed(2);
+            p.profit = (p.currentValue - p.purchaseValue).toFixed(2);
+            p.totalChange = ((p.currentPrice/p.purchasePrice - 1) * 100).toFixed(2);
+            p.dailyChange = prices.data.data[p.symbol].quote.USD.percent_change_24h.toFixed(2);
+        }
         res.render("pages/index",{positions: positions.data});
     } catch (error) {
         console.error(error);
